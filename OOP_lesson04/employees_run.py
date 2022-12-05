7ace8bd564ae2d89c061cec3cc5f61f69623105e
Polina <theidolsinside@gmail.com>
2022-12-05 12:43:23 +0000

    oop_homework

--- OOP_lesson04/employees_run.py
+++ OOP_lesson04/employees_run.py
@@ -13,5 +13,6 @@ if __name__ == '__main__':
     try:
         main()
     except EmailAlreadyExistsException as err:
+        print('Email already exists!')
         with open('/home/polina/Документи/polina/homework/OOP_lesson04/logs', 'a+') as file:
             file.write(traceback.format_exc())
